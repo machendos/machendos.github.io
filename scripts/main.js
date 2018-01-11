d183318cabfab64c70c5e515d4c3ca592e1d688f
Nicita01 <nicitamac@gmail.com>
2018-01-12 01:30:56 +0200

    add test-site

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML  = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML  = 'Mozilla is cool, ' + storedName;
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+myButton.onclick = function() {
+  setUserName();
+}
+
